b69e25d6dc0a6342b485173d78700489d1aeabc7
Kevin Perez <kevindperezm@gmail.com>
2014-06-02 22:45:03 -0500

    add fill_select.js with get_by_ajax() function

---
+++ scripts/fill_select.js
@@ -0,0 +1,28 @@
+function get_by_ajax(resource_url, success_callback) {
+  var xmlhttp;
+
+  if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+  } else {
+    // code for IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 ) {
+      if(xmlhttp.status == 200) {
+        success_callback(xmlhttp.responseText);
+      } else if(xmlhttp.status == 404) {
+        // TODO: Callback for Not Found
+        console.log(xmlhttp.responseText);
+      } else {
+        // TODO: Callback for anything else
+        console.log(xmlhttp.responseText);
+      }
+    }
+  }
+
+  xmlhttp.open('GET', resource_url, true);
+  xmlhttp.send();
+}
